408d0cbfb3fd3186e5fbfdd7ef674a50993588c1
shaunculrich77 <sulrich@doctorondemand.com>
2021-04-07 13:25:08 -0400

    modify file structure

---
+++ src/modules/sample-module/pages/about/about.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="about">
+        <h2>This is an about page</h2>
+    </div>
+</template>
+
+<script lang="js" src="./about.js"></script>
+<style lang="scss" src="./about.scss"></style>
---
+++ src/modules/sample-module/pages/index/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="v-cloak">
+        <img
+            class="img-fluid dod-logo"
+            alt="Doctor on Deman"
+            src="@/assets/DoD_logo.png"
+        />
+        <h2>External Integrations Web Template</h2>
+    </div>
+</template>
+
+<script lang="js" src="./index.js"></script>
+<style lang="scss" src="./index.scss"></style>
