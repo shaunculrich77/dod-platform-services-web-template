93ff2f84d44bef4bf317644adb62091a483eb482
shaunculrich77 <sulrich@doctorondemand.com>
2021-04-08 13:55:43 -0400

    change localhost name and port

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,13 @@ const fs = require('fs');
 module.exports = {
     devServer: {
         https: {
-            key: fs.readFileSync('./certs/localhost-key.pem'),
-            cert: fs.readFileSync('./certs/localhost.pem'),
+            key: fs.readFileSync('./certs/local.dod.la-key.pem'),
+            cert: fs.readFileSync('./certs/local.dod.la.pem'),
         },
+        host: 'local.dod.la',
+        port: 8085,
+
         // added to stop ERR_CONNECTION_REFUSED error. Error because cert is not issued by an acceptable CA and Chrome complains
-        public: 'https://localhost:8080/',
+        //public: 'https://localhost:8080/',
     },
 };
